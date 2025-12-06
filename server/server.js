0a6a84bfe8e2857a59486453326871fd960d2dab
Himanshu Chhabra <himanshuchhabradev@gmail.com>
2025-12-06 21:20:12 +0530

    middleware created

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server started at ${PORT}`)
